b7a0f5e1c6f17fe06f9c446d6a4038ddc3c887fe
atipune <atipune.soft@gmail.com>
2025-06-23 08:15:05 +0530

    login module script

--- SampleProject/src/demo/NumberAddition.java
+++ SampleProject/src/demo/NumberAddition.java
@@ -3,8 +3,7 @@ package demo;
 public class NumberAddition {
 	public static void main(String[] args) {
 		int a=10;
-		int b=20;
-		int sum=a+b;
-		System.out.println(sum);
+		int b=20;;
+		int sum=a+b; //hello sample comment
 	}
 }
---
+++ SampleProject/src/demo/SumberSubtraction.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class SumberSubtraction {
+	public static void main(String[] args) {
+		int c=20;
+		int d=10;
+		int sub=c-d;
+		System.out.println(sub);
+	}
+}
